cf048ceec3c9135434195c71e71c480b3065377a
unknown <godsonemmanuel07@gmail.com>
2021-10-02 20:59:58 +0530

    added login and logout functionality

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -16,14 +16,7 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-        // dd($request);
         // Validation
-        // $this->validate($request, [
-        //     'name' => 'required|max:255',
-        //     'username' => 'required|max:255',
-        //     'email' => 'required|email|max:255',
-        //     'password' => 'required|confirmed'   // godson@123
-        // ]);
         $this->validate($request, [
             'name' => ['required', 'max:255'],
             'username' => ['required', 'max:255'],
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,25 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/posts', function () {
-//     return view('posts.index');
-// });
+Route::get('/', function () {
+    return view('home');
+})->name('home');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/', function () {
+Route::get('/posts', function () {
     return view('posts.index');
-})->name('home');
+});
